4f08e117e805bf53786a97df2d423b2e453bc546
DanielDeveloper <danielfelipedeveloper@gmail.com>
2020-11-06 15:31:04 -0400

    fix: edit page

--- src/app/controllers/admin-controller.js
+++ src/app/controllers/admin-controller.js
@@ -1,16 +1,34 @@
-const recipes = require('../../data.json');
+const data = require('../../data.json');
+const recipes = data.recipes;
+
 module.exports = {
   index(req, res) {
-    return res.render('admin/index', { recipes: recipes.recipes })
+    return res.render('admin/index', { recipes })
   },
   create(req, res) {
     return res.render('admin/create')
   },
   show(req, res) {
-    
+    const recipeId = req.params.id
+
+    const foundRecipe = recipes.find((recipe) => {
+      return recipe.id == recipeId
+    })
+
+    if (!foundRecipe) return 'Recipe not found!'
+
+    return res.render('admin/show', { recipe : recipes[foundRecipe.id - 1] })
   },
   edit(req, res) {
-    return res.render('admin/edit')
+    const recipeId = req.params.id
+
+    const foundRecipe = recipes.find((recipe) => {
+      return recipe.id == recipeId
+    })
+
+    if (!foundRecipe) return 'Recipe not found!'
+
+    return res.render('admin/edit', { recipe: recipes[foundRecipe.id - 1] })
   },
   // HTTP METHODS //
   post(req, res) {
