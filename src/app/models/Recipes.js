789e118d3fe6d9c33e27f45d48f067fdfe3e36f6
Daniel Felipe Developer <danielfelipedeveloper@gmail.com>
2020-12-02 20:19:08 -0400

    feat: creating recipes with chef

--- src/app/models/Recipes.js
+++ src/app/models/Recipes.js
@@ -15,19 +15,21 @@ module.exports = {
   create(data, callback) {
     const query = `
       INSERT INTO recipes (
-        image,
         title,
+        image,
+        chef_id,
         ingredients,
         preparation,
         information,
         created_at
-      ) VALUES ($1, $2, $3, $4, $5, $6)
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7)
       RETURNING id
     `
 
     const values = [
-      data.image,
       data.title,
+      data.image,
+      Number(data.chef_id),
       data.ingredients,
       data.preparation,
       data.information,
